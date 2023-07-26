41c84ef03e685c60eb0c26138e04f6631d91e3ab
RajobAli <freelancerrajobali@gmail.com>
2023-07-26 23:14:44 +0600

    added image6

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelector("#btnone").addEventListener("click",function myFunction(){
+
+
+});
+
+
+var myvar = document.querySelector("#mmm");
+myvar.addEventListener("click",function(){
+myvar.classList.add("my-style");
+    
+
+});
+
+
+
+
+
+
+
+
